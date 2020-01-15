dd8b2616246cc4494a2b8f3ef298a2e38e1f86be
Fadima <manenga@gmail.com>
2020-01-15 01:07:43 +0100

    composant JSX

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css'
+import GuessCount from './GuessCount'
+import Card from './Card'
+
+class App extends Component {
+  render() {
+    return (
+        <div className="memory">
+          <GuessCount guesses={0} />
+          <Card card=":)" feedback="hidden" />
+          <Card card=":D" feedback="justMatched" />
+          <Card card=":(" feedback="justMismatched" />
+          <Card card=":/" feedback="visible" />
+          <Card card=":O" feedback="hidden" />
+          <Card card=":p" feedback="justMatched" />
+        </div>
+      )
+  }
 }
 
-export default App;
+export default App
