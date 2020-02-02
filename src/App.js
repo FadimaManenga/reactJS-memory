099b1205adcf366d5215591e9f685a574b001d9f
Fadima <manenga@gmail.com>
2020-02-02 21:56:47 +0100

    onClick event

--- src/App.js
+++ src/App.js
@@ -5,19 +5,27 @@ import GuessCount from './GuessCount'
 import Card from './Card'
 
 class App extends Component {
-  render() {
-    return (
-        <div className="memory">
-          <GuessCount guesses={0} />
-          <Card card=":)" feedback="hidden" />
-          <Card card=":D" feedback="justMatched" />
-          <Card card=":(" feedback="justMismatched" />
-          <Card card=":/" feedback="visible" />
-          <Card card=":O" feedback="hidden" />
-          <Card card=":p" feedback="justMatched" />
-        </div>
-      )
+  handleCardClick(card) {
+    console.log(card, "clicked")
   }
+    render() {
+      return (
+        // 4 possibilités de feedback
+          // hidden = paire non complétée
+          // visible = paire déjà complétée avec succès
+          // justMatched = paire complétée avec succès
+          // justMismatched = paire fausse
+          <div className="memory">
+            <GuessCount guesses={0} />
+            <Card card=":)" feedback="hidden" onClick={this.handleCardClick} />
+            <Card card=":D" feedback="justMatched" onClick ={this.handleCardClick} />
+            <Card card=":(" feedback="justMismatched" onClick ={this.handleCardClick} />
+            <Card card=":/" feedback="visible" onClick ={this.handleCardClick}/>
+            <Card card=":O" feedback="hidden" onClick ={this.handleCardClick} />
+            <Card card=":p" feedback="justMatched" onClick ={this.handleCardClick} />
+          </div>
+        )
+    }
 }
 
 export default App
--- src/Card.js
+++ src/Card.js
@@ -5,10 +5,13 @@ import './Card.css'
 const HIDDEN_SYMBOL = '❓'
 
 // destructuration de props (informations) : card, feedback
-const Card = ({ card, feedback }) => (
-    <div className={`card ${feedback}`}>
-        <span className="symbol">
-            {feedback === "hidden" ? HIDDEN_SYMBOL : card}
+    // props card : type du symbole
+    // props feedback : état d'affichage du symbole
+const Card = ({ card, feedback, onClick }) => (
+    // grappe enrobée par des parenthèses : ()
+    <div className={`card ${feedback}`} onClick={() => onClick(card)}>
+            <span className="symbol">
+            {feedback === "hidden" ? HIDDEN_SYMBOL : card} 
         </span>    
     </div>
 )
