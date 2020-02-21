cffc54670c36f6a4e055e332c6c0b8480dc99eec
Fadima <manenga@gmail.com>
2020-02-21 01:53:40 +0100

    test avec Enzyme Airbnb

--- src/App.spec.js
+++ src/App.spec.js
@@ -1,14 +1,36 @@
+import {expect} from "chai"
+import {shallow } from "enzyme"
 import React from "react"
-import ReactDom from "react-dom"
+// import ReactDom from "react-dom"
+
 import App from "./App"
+import GuessCount from "./GuessCount"
+
+
+describe('<App />', () => {
 
 // it()  est fourni par Jest. Nous n’avons pas besoin de recourir à une assertion spécifique, car c’est le  ReactDOM.render()  qui lèvera une exception s’il rencontre une erreur
-it("renders without crashing", () => {
-    const div = document.createElement("div")
-    ReactDom.render(<App />, div)
-})
+    it("renders without crashing", () => {
+        /*
+        const div = document.createElement("div")
+        ReactDom.render(<App />, div)
+        */
+        const wrapper = shallow(<App />)
 
+        expect(wrapper).to.contain(<GuessCount guesses={0} />)
+    })
+
+    it("has 36 cards", () => {
+        const wrapper = shallow(<App />)
+
+        expect(wrapper.find("card")).to.have.length(36)
+    })
+
+});
+
+/*
 // test chai
 it("demoes", () => {
     expect({name: "Joe"}).toEqual({name: "Jane"})
 })
+*/
--- src/setupTests.js
+++ src/setupTests.js
@@ -4,9 +4,18 @@
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
 
+import Adapter from "enzyme-adapter-react-16"
 import chai from "chai"
-import dirtyChai from "dirty-chai"
+import {configure as configureEnzyme} from "enzyme"
 import createChaiJestDiff from "chai-jest-diff"
+import createChaiEnzyme from "chai-enzyme"
+import dirtyChai from "dirty-chai"
 
-chai.use(dirtyChai).use(createChaiJestDiff())
+chai
+.use(dirtyChai)
+.use(createChaiJestDiff())
+.use(createChaiEnzyme())
+
+configureEnzyme({adapter: new Adapter()})
+
 
